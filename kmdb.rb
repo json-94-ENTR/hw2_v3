555b3893bd1a7341ad781eff3c6806a9e1dba8ee
Janu Sonthi <janu.sonthi@kellogg.northwestern.edu>
2023-05-04 14:21:14 +0000

    inserted_data_into_studio_table

--- kmdb.rb
+++ kmdb.rb
@@ -70,6 +70,10 @@
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
 # TODO!
+Studio.destroy_all
+Movie.destroy_all
+Actor.destroy_all
+Role.destroy_all
 
 # Generate models and tables, according to the domain model.
 # TODO!
@@ -77,6 +81,11 @@
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
 # TODO!
+
+#Insert data into studio table
+new_studio = Studio.new
+new_studio ["name"] = "Warner Bros."
+new_studio.save
 
 # Prints a header for the movies output
 puts "Movies"
